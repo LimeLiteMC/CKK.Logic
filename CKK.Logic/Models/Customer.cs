a5f9c3bd2c20f579b000ff0abe66b5dd7b6c46c1
Nathan Stevenson <Natejon13@gmail.com>
2022-10-24 21:41:54 -0600

    Add project files.

---
+++ CKK.Logic/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CKK.Logic.Models
+{
+    internal class Customer
+    {
+        private int Id;
+        private string Name = "";
+        private string Address = "";
+        public int GetID()
+        {
+            return Id;
+        }
+        public void SetID(int id)
+        {
+            Id = id;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public string GetAddress()
+        {
+            return Address;
+        }
+        public void SetAddress(string address)
+        {
+            Address = address;
+        }
+
+    }
+
+}
---
+++ CKK.Logic/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CKK.Logic.Models
+{
+    internal class Product
+    {
+        private int Id = 0;
+        private string Name = "";
+        private decimal Price = 0.00m;
+        public int GetId()
+        {
+            return Id;
+        }
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+        public void SetPrice(decimal price)
+        {
+            Price = price;
+        }
+    }
+}
